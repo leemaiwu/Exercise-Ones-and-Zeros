53a9d706d17d7df1d2b1640a65901e94631773ff
Laura Wu <leemaiwu@gmail.com>
2023-06-21 11:05:02 -0600

    Completed with for loop, splice, and reduce method

---
+++ solution.js
@@ -0,0 +1,14 @@
+const binaryArrayToNumber = arr => {
+    arr = arr.reverse()
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr.splice(i, 1, (2 ** i))
+        }
+    }
+    return arr.reduce((a, b) => a + b, 0)
+}
+
+console.log(binaryArrayToNumber([0, 0, 0, 1])) // 1
+console.log(binaryArrayToNumber([0, 0, 1, 0])) // 2
+console.log(binaryArrayToNumber([1, 0, 0, 1])) // 9
+console.log(binaryArrayToNumber([1, 1, 1, 1])) // 15
