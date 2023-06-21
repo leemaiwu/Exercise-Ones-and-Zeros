cb383ed5f2c6331741d1225d94ad570468842195
Laura Wu <leemaiwu@gmail.com>
2023-06-21 11:11:54 -0600

    Added alternate parseint solution

--- solution.js
+++ solution.js
@@ -12,3 +12,12 @@ console.log(binaryArrayToNumber([0, 0, 0, 1])) // 1
 console.log(binaryArrayToNumber([0, 0, 1, 0])) // 2
 console.log(binaryArrayToNumber([1, 0, 0, 1])) // 9
 console.log(binaryArrayToNumber([1, 1, 1, 1])) // 15
+
+// Alternate Solution
+
+const binaryArrayToDecimal = arr => parseInt(arr.join(''), 2)
+
+console.log(binaryArrayToDecimal([0, 0, 0, 1])) // 1
+console.log(binaryArrayToDecimal([0, 0, 1, 0])) // 2
+console.log(binaryArrayToDecimal([1, 0, 0, 1])) // 9
+console.log(binaryArrayToDecimal([1, 1, 1, 1])) // 15
